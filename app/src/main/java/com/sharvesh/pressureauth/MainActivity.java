8579eda98566a719009095a8f2b7cf5f9972d198
AjaySharvesh3 <ajaysharvesh.mp@gmail.com>
2019-12-07 20:23:44 +0530

    Initial commit

---
+++ app/src/main/java/com/sharvesh/pressureauth/MainActivity.java
@@ -0,0 +1,262 @@
+package com.sharvesh.pressureauth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonOne;
+    private Button buttonTwo;
+    private Button buttonThree;
+    private Button buttonFour;
+    private Button buttonFive;
+    private Button buttonSix;
+    private Button buttonSeven;
+    private Button buttonEight;
+    private Button buttonNine;
+    private Button clearButton;
+
+    private Button confirmCodeButton;
+
+    private EditText textOne;
+    private EditText texttwo;
+    private StringBuilder pressureValue;
+    private StringBuilder actualValue;
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        pressureValue = new StringBuilder();
+        actualValue = new StringBuilder();
+
+        buttonOne = findViewById(R.id.buttonOne);
+        buttonTwo = findViewById(R.id.buttonTwo);
+        buttonThree = findViewById(R.id.buttonThree);
+        buttonFour = findViewById(R.id.buttonFour);
+        buttonFive = findViewById(R.id.buttonFive);
+        buttonSix = findViewById(R.id.buttonSix);
+        buttonSeven = findViewById(R.id.buttonSeven);
+        buttonEight = findViewById(R.id.buttonEight);
+        buttonNine = findViewById(R.id.buttonNine);
+        clearButton = findViewById(R.id.buttonClear);
+
+        confirmCodeButton = findViewById(R.id.confirmCodeButton);
+
+        textOne = findViewById(R.id.valueOne);
+        texttwo = findViewById(R.id.valueTwo);
+
+        final long[] lastDown = new long[1];
+        final long[] keyPressedDuration = new long[1];
+
+        buttonOne.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                    lastDown[0] = System.currentTimeMillis();
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    keyPressedDuration[0] = System.currentTimeMillis() - lastDown[0];
+                    if (keyPressedDuration[0] < 200) {
+                        textOne.setText(pressureValue.append("1"));
+                        texttwo.setText(actualValue.append("1"));
+                    } else if (keyPressedDuration[0] > 200){
+                        textOne.setText(pressureValue.append("2"));
+                        texttwo.setText(actualValue.append("1"));
+                    }
+                }
+                return true;
+            }
+        });
+
+        buttonTwo.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                    lastDown[0] = System.currentTimeMillis();
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    keyPressedDuration[0] = System.currentTimeMillis() - lastDown[0];
+                    if (keyPressedDuration[0] < 200) {
+                        textOne.setText(pressureValue.append("1"));
+                        texttwo.setText(actualValue.append("2"));
+                    } else if (keyPressedDuration[0] > 200){
+                        textOne.setText(pressureValue.append("2"));
+                        texttwo.setText(actualValue.append("2"));
+                    }
+                }
+                return true;
+            }
+        });
+
+        buttonThree.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                    lastDown[0] = System.currentTimeMillis();
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    keyPressedDuration[0] = System.currentTimeMillis() - lastDown[0];
+                    if (keyPressedDuration[0] < 200) {
+                        textOne.setText(pressureValue.append("1"));
+                        texttwo.setText(actualValue.append("3"));
+                    } else if (keyPressedDuration[0] > 200){
+                        textOne.setText(pressureValue.append("2"));
+                        texttwo.setText(actualValue.append("3"));
+                    }
+                }
+                return true;
+            }
+        });
+
+        buttonFour.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                    lastDown[0] = System.currentTimeMillis();
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    keyPressedDuration[0] = System.currentTimeMillis() - lastDown[0];
+                    if (keyPressedDuration[0] < 200) {
+                        textOne.setText(pressureValue.append("1"));
+                        texttwo.setText(actualValue.append("4"));
+                    } else if (keyPressedDuration[0] > 200){
+                        textOne.setText(pressureValue.append("2"));
+                        texttwo.setText(actualValue.append("4"));
+                    }
+                }
+                return true;
+            }
+        });
+
+        buttonFive.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                    lastDown[0] = System.currentTimeMillis();
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    keyPressedDuration[0] = System.currentTimeMillis() - lastDown[0];
+                    if (keyPressedDuration[0] < 200) {
+                        textOne.setText(pressureValue.append("1"));
+                        texttwo.setText(actualValue.append("5"));
+                    } else if (keyPressedDuration[0] > 200){
+                        textOne.setText(pressureValue.append("2"));
+                        texttwo.setText(actualValue.append("5"));
+                    }
+                }
+                return true;
+            }
+        });
+
+        buttonSix.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                    lastDown[0] = System.currentTimeMillis();
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    keyPressedDuration[0] = System.currentTimeMillis() - lastDown[0];
+                    if (keyPressedDuration[0] < 200) {
+                        textOne.setText(pressureValue.append("1"));
+                        texttwo.setText(actualValue.append("6"));
+                    } else if (keyPressedDuration[0] > 200){
+                        textOne.setText(pressureValue.append("2"));
+                        texttwo.setText(actualValue.append("6"));
+                    }
+                }
+                return true;
+            }
+        });
+
+        buttonSeven.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                    lastDown[0] = System.currentTimeMillis();
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    keyPressedDuration[0] = System.currentTimeMillis() - lastDown[0];
+                    if (keyPressedDuration[0] < 200) {
+                        textOne.setText(pressureValue.append("1"));
+                        texttwo.setText(actualValue.append("7"));
+                    } else if (keyPressedDuration[0] > 200){
+                        textOne.setText(pressureValue.append("2"));
+                        texttwo.setText(actualValue.append("7"));
+                    }
+                }
+                return true;
+            }
+        });
+
+        buttonEight.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                    lastDown[0] = System.currentTimeMillis();
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    keyPressedDuration[0] = System.currentTimeMillis() - lastDown[0];
+                    if (keyPressedDuration[0] < 200) {
+                        textOne.setText(pressureValue.append("1"));
+                        texttwo.setText(actualValue.append("8"));
+                    } else if (keyPressedDuration[0] > 200){
+                        textOne.setText(pressureValue.append("2"));
+                        texttwo.setText(actualValue.append("8"));
+                    }
+                }
+                return true;
+            }
+        });
+
+        buttonNine.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                    lastDown[0] = System.currentTimeMillis();
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    keyPressedDuration[0] = System.currentTimeMillis() - lastDown[0];
+                    if (keyPressedDuration[0] < 200) {
+                        textOne.setText(pressureValue.append("1"));
+                        texttwo.setText(actualValue.append("9"));
+                    } else if (keyPressedDuration[0] > 200){
+                        textOne.setText(pressureValue.append("2"));
+                        texttwo.setText(actualValue.append("9"));
+                    }
+                }
+                return true;
+            }
+        });
+
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int length = actualValue.length();
+                try {
+                    String actualValue1 = actualValue.substring(0, length-1);
+                    actualValue.replace(0, length, actualValue1);
+                    texttwo.setText(actualValue);
+
+                    String pressureValue1 = pressureValue.substring(0, length-1);
+                    pressureValue.replace(0, length, pressureValue1);
+                    textOne.setText(pressureValue);
+                } catch (StringIndexOutOfBoundsException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+        confirmCodeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent confirmCodeActivityIntent = new Intent(MainActivity.this, ConfirmPassCodeActivity.class);
+                confirmCodeActivityIntent.putExtra("Actual Value", actualValue.toString());
+                confirmCodeActivityIntent.putExtra("PassCode Value", pressureValue.toString());
+                startActivity(confirmCodeActivityIntent);
+            }
+        });
+    }
+}
